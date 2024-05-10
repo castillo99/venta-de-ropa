607aa8967788124b1eab02e40187c7e2d1372c59
castillo99 <103867521+castillo99@users.noreply.github.com>
2024-05-10 00:20:11 -0500

    Update data.js

--- data.js
+++ data.js
@@ -1,15 +1,1 @@
-const productos = [
-    {
-        nombre: "Producto 1",
-        categoria: "Categoría 1",
-        imagen: "imagen1.jpg",
-        codigo: "ABC123",
-        precio: 10.99,
-        atributos: {
-            atributo1: "Valor 1",
-            atributo2: "Valor 2",
-            // Añade más atributos según sea necesario
-        }
-    },
-    // Otros productos
-];
+
